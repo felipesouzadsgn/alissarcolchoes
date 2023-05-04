5e285847d5f8175180f4067ef14366b2dec28ca9
Felipe Souza <felipesouzadsgn@gmail.com>
2023-05-04 14:29:55 -0300

    Updating code

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,12 +3,12 @@ import LogoAlissar from "../assets/svg/LogoAlissar.svg"
 
 export function Header() {
   return (
-    <header className="w-full fixed left-0 top-0 flex justify-center items-center text-white">
-      <nav className="w-full max-w-7xl h-20 flex items-center justify-between">
+    <header className="w-full fixed z-10 left-0 top-0 flex justify-center items-center text-white">
+      <nav className="w-full max-w-7xl h-20 flex items-center justify-between max-md:justify-center max-md:p-3">
         <a href="">
           <Image src={LogoAlissar} alt="Logo Alissar Colchões" />
         </a>
-        <ul className="flex gap-8 ">
+        <ul className="flex gap-8 max-md:hidden ">
           <li>
             <a href="">Home</a>
           </li>
@@ -22,7 +22,7 @@ export function Header() {
             <a href="">Contato</a>
           </li>
         </ul>
-        <a href="" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</a>
+        <a href="" className="py-3 px-6 rounded bg-slate-50 text-blue-500 max-md:hidden">Solicitar Orçamento</a>
       </nav>
     </header>
   )
