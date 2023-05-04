3eb684106a84db21dbd34f80569945d01f3aeb34
Felipe Souza <felipesouzadsgn@gmail.com>
2023-05-04 13:54:02 -0300

    Updating code

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -67,35 +67,35 @@ export function Hero() {
   return (
     <section className="relative navigation-wrapper">
       <ul className="absolute left-6 top-1/2 -translate-y-1/2 flex flex-col gap-6 z-10">
-        <li><Image src={InstagramLogo} alt="Icone do Instagram" /></li>
-        <li><Image src={FacebookLogo} alt="Icone do Facebook" /></li>
-        <li><Image src={WhatsAppLogo} alt="Icone do WhatsApp" /></li>
+        <li><Image src={InstagramLogo} alt="Icone do Instagram" className="w-8 h-8 max-md:w-6 max-md:h-5" /></li>
+        <li><Image src={FacebookLogo} alt="Icone do Facebook" className="w-8 h-8 max-md:w-6 max-md:h-5" /></li>
+        <li><Image src={WhatsAppLogo} alt="Icone do WhatsApp" className="w-8 h-8 max-md:w-6 max-md:h-5" /></li>
       </ul>
       <div ref={sliderRef} className="keen-slider">
         <div className="keen-slider__slide number-slide1 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
           <Image src={IMG1} alt="Image de Colchão & Box" className="w-[624px] 6-[524px]" />
           <div className="max-w-md max-tablet:text-center">
             <span className="uppercase text-white">Category</span>
-            <h2 className="text-8xl bold max-tablet:text-center text-yellow-400 max-tablet:text-7xl">Baú Casal</h2>
-            <p className="text-white my-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
+            <h2 className="text-7xl mt-2 max-md:text-5xl bold max-tablet:text-center text-yellow-400">Baú Casal</h2>
+            <p className="text-white max-md:text-sm my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
             <a href="" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</a>
           </div>
         </div>
-        <div className="keen-slider__slide number-slide2 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
+        <div className="keen-slider__slide number-slide1 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
           <Image src={IMG2} alt="Image de Colchão & Box" className="w-[624px] 6-[524px]" />
           <div className="max-w-md max-tablet:text-center">
             <span className="uppercase text-white">Category</span>
-            <h2 className="text-8xl bold max-tablet:text-center text-yellow-400 max-tablet:text-7xl">Colchão Casal</h2>
-            <p className="text-white my-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
+            <h2 className="text-7xl mt-2 max-md:text-5xl bold max-tablet:text-center text-yellow-400">Baú Casal</h2>
+            <p className="text-white max-md:text-sm my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
             <a href="" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</a>
           </div>
         </div>
-        <div className="keen-slider__slide number-slide2 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
+        <div className="keen-slider__slide number-slide1 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
           <Image src={IMG3} alt="Image de Colchão & Box" className="w-[624px] 6-[524px]" />
           <div className="max-w-md max-tablet:text-center">
             <span className="uppercase text-white">Category</span>
-            <h2 className="text-8xl bold max-tablet:text-center text-yellow-400 max-tablet:text-7xl">Colchão & Box</h2>
-            <p className="text-white my-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
+            <h2 className="text-7xl mt-2 max-md:text-5xl bold max-tablet:text-center text-yellow-400">Baú Casal</h2>
+            <p className="text-white max-md:text-sm my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
             <a href="" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</a>
           </div>
         </div>
