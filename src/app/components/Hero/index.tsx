637b8ddc6a4ddd6cf1e70d6005e4b824752d3d7d
Felipe Souza <felipesouzadsgn@gmail.com>
2023-05-18 16:14:12 -0300

    Updating code

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -17,21 +17,6 @@ import IMG3 from "../../assets/img/Img001.png"
 import { useState } from "react"
 import Link from "next/link"
 
-export const gtagReportConversion = (url: any) => {
-  const callback = () => {
-    if (typeof url !== 'undefined') {
-      window.location = url;
-    }
-  };
-
-  globalThis.gtag('event', 'conversion', {
-    send_to: 'AW-773499482/gU1vCInImKEYENrU6vAC',
-    event_callback: callback,
-  });
-
-  return false;
-};
-
 
 export function Hero() {
   const [currentSlide, setCurrentSlide] = useState(0)
@@ -83,7 +68,7 @@ export function Hero() {
       <ul className="absolute left-6 top-1/2 -translate-y-1/2 flex flex-col gap-6 max-md:gap-3 z-10">
         <li><Link href="https://instagram.com/alissarcolchoes" target="_blank"><Image src={InstagramLogo} alt="Icone do Instagram" className="w-8 h-8 max-md:w-6 max-md:h-5" /></Link></li>
         <li><Link href="https://facebook.com/alissarcolchoes" target="_blank"><Image src={FacebookLogo} alt="Icone do Instagram" className="w-8 h-8 max-md:w-6 max-md:h-5" /></Link></li>
-        <li><Link onClick={() => gtagReportConversion('')} href="https://wa.me/5513981577284" target="_blank"><Image src={WhatsAppLogo} alt="Icone do Instagram" className="w-8 h-8 max-md:w-6 max-md:h-5" /></Link></li>
+        <li><Link href="https://wa.me/5513981577284" target="_blank"><Image src={WhatsAppLogo} alt="Icone do Instagram" className="w-8 h-8 max-md:w-6 max-md:h-5" /></Link></li>
       </ul>
       <div ref={sliderRef} className="keen-slider">
         <div className="keen-slider__slide number-slide1 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
@@ -92,7 +77,7 @@ export function Hero() {
             <span className="uppercase text-white">Category</span>
             <h2 className="text-7xl mt-2 max-md:text-5xl bold max-tablet:text-center text-yellow-400">Baú Casal</h2>
             <p className="text-white max-md:text-sm my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus a at suscipit nemo eveniet fugiat itaque possimus tempora.</p>
-            <Link onClick={() => gtagReportConversion('')} href="https://wa.me/5513981577284" target="_blank" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</Link>
+            <Link href="https://wa.me/5513981577284" target="_blank" className="py-3 px-6 rounded bg-slate-50 text-blue-500">Solicitar Orçamento</Link>
           </div>
         </div>
         <div className="keen-slider__slide number-slide2 flex flex-wrap justify-center items-center gap-3 max-tablet:flex-col w-full h-screen max-tablet:h-full bg-blue-700 p-12">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,20 @@ export default function RootLayout({
           gtag('config', 'AW-773499482');
         `}
         </Script>
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P57J3BJ');
+        `}
+        </Script>
         <Header />
         {children}</body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P57J3BJ"
+        className='hidden invisible w-0 h-0'></iframe></noscript>
     </html>
   )
 }
