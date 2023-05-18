2ee969b7a444c595cb8b7827138a1f3691986198
Felipe Souza <felipesouzadsgn@gmail.com>
2023-05-18 16:16:33 -0300

    Updating code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         `}
         </Script>
 
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script strategy="afterInteractive">
           {`
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
