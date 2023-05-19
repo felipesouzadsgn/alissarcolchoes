fe2e420458af0ab3735dd7b8485a6e9540c2cc1b
Felipe Souza <felipesouzadsgn@gmail.com>
2023-05-19 10:32:20 -0300

    Updating code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import Head from 'next/head'
 import { Header } from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-773499482"
           strategy="afterInteractive"
@@ -41,11 +42,29 @@ export default function RootLayout({
       })(window,document,'script','dataLayer','GTM-P57J3BJ');
         `}
         </Script>
+
+      <Script id='meta-pixel'>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2249326158581139');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <body className={inter.className}>
         <Header />
         {children}
-
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P57J3BJ"
           className='hidden invisible w-0 h-0'></iframe></noscript>
+        <noscript><Image height="1" width="1" className='hidden'
+          src="https://www.facebook.com/tr?id=2249326158581139&ev=PageView&noscript=1" alt=''
+        /></noscript>
       </body>
     </html>
   )
